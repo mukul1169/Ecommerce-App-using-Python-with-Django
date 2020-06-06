1dce858ad344612e719a7e57bfd9c0c3d57ada4c
mukul1169 <mchaudhary.cse17@chitkarauniversity.edu.in>
2020-06-06 15:26:25 +0530

    Ecommerce App

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\LENOVE\PycharmProjects\Ecommerce_Website\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
